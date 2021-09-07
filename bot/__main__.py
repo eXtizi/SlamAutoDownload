01bacdc7bba0501933d8662c10114b5b69b587eb
vidushan0 <80735884+vidushan0@users.noreply.github.com>
2021-09-07 16:27:05 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -64,7 +64,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
     else :
         sendMarkup(f"Oops! not a Authorized user.\nPlease deploy your own <b>slam-tg-mirror-bot</b>.", context.bot, update, reply_markup)
 
-async def files_handler(update,context ):
+def fileshandler(update,context ):
         print('file')
         link=''
         file = None
@@ -252,14 +252,14 @@ def main():
     stats_handler = CommandHandler(BotCommands.StatsCommand,
                                    stats, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
     log_handler = CommandHandler(BotCommands.LogCommand, log, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
-    
+    files_handler = MessageHandler(fileshandler,filters.video|filters.document|filters.audio),-1)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(ping_handler)
     dispatcher.add_handler(restart_handler)
     dispatcher.add_handler(help_handler)
     dispatcher.add_handler(stats_handler)
     dispatcher.add_handler(log_handler)
-    dispatcher.add_handler(files_handler,filters=filters.video|filters.document|filters.audio)
+    dispatcher.add_handler(files_handler,filters=)
     updater.start_polling(drop_pending_updates=IGNORE_PENDING_REQUESTS)
     LOGGER.info("Bot Started!")
     signal.signal(signal.SIGINT, fs_utils.exit_clean_up)
