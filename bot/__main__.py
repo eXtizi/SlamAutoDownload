3bb11ca5233276e52b8f2d91de70bb5c5f62cd96
vidushan0 <80735884+vidushan0@users.noreply.github.com>
2021-09-07 19:04:21 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -254,7 +254,7 @@ def main():
                                    stats, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
     log_handler = CommandHandler(BotCommands.LogCommand, log, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
     #filters1=filters.media
-    files_handler = MessageHandler((filters.document),fileshandler, run_async=True)
+    files_handler = MessageHandler((filters.all),fileshandler, run_async=True)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(ping_handler)
     dispatcher.add_handler(restart_handler)
